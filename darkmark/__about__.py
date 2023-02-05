3524f17de1853bb3a86b2f5ae6e9bb53dab63a85
Waylon S. Walker <waylon@waylonwalker.com>
2023-02-05 13:44:14 -0600

    make releasable version

--- darkmark/__about__.py
+++ darkmark/__about__.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2023-present Waylon S. Walker <waylon@waylonwalker.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
+__version__ = "0.0.1.dev0"
 name = "darkmark"
