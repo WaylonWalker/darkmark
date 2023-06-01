25b4f7308035cbadaff02f9a60629f4e9173a153
autobump <autobump@users.noreply.github.com>
2023-06-01 22:30:34 +0000

    Bump version: 0.2.0.dev0 → 0.2.0

--- darkmark/__about__.py
+++ darkmark/__about__.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2023-present Waylon S. Walker <waylon@waylonwalker.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "0.2.0.dev0"
+__version__ = "0.2.0"
 name = "darkmark"
